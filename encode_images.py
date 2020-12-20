ffcf67f8e098c442efbec1c2a8e61f3a0689fe1b
Lim Yee Han <limyeehan@gmail.com>
2020-12-20 19:20:18 +0800

    vgg16

--- encode_images.py
+++ encode_images.py
@@ -125,7 +125,8 @@ def main():
 
     perc_model = None
     if (args.use_lpips_loss > 0.00000001):
-        with dnnlib.util.open_url('https://drive.google.com/uc?id=1N2-m9qszOeVC9Tq77WxsLnuWwOedQiD2', cache_dir=config.cache_dir) as f:
+        # with dnnlib.util.open_url('https://drive.google.com/uc?id=1N2-m9qszOeVC9Tq77WxsLnuWwOedQiD2', cache_dir=config.cache_dir) as f:
+        with open('models/vgg16_zhang_perceptual (1).pkl', 'rb') as f:
             perc_model =  pickle.load(f)
     perceptual_model = PerceptualModel(args, perc_model=perc_model, batch_size=args.batch_size)
     perceptual_model.build_perceptual_model(generator, discriminator_network)
